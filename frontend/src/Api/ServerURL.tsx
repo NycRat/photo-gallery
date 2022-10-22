d241c70ebe385e6fca136788fbff4570725653f7
Eric Xiao <ericxiao0000@gmail.com>
2022-10-22 14:40:42 -0700

    updated server url

--- frontend/src/Api/ServerURL.tsx
+++ frontend/src/Api/ServerURL.tsx
@@ -1,4 +1,6 @@
 const ServerURL =
-  process.env.NODE_ENV === "production" ? "https://photo-gallery-backend.herokuapp.com/api" : "http://localhost:8000/api";
+  process.env.NODE_ENV === "production"
+    ? "https://container-service-1.vn5jercmc73d4.us-west-2.cs.amazonlightsail.com/api/photo_gallery"
+    : "http://localhost:8000/api/photo_gallery";
 
 export default ServerURL;
