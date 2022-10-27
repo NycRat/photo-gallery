8f3d90f40e2103254bf6d8f84bc6248f31ca10fe
Eric Xiao <ericxiao0000@gmail.com>
2022-10-26 19:53:26 -0700

    fix server url, and 3/2 aspect ratio for all image

--- frontend/src/Api/ServerURL.tsx
+++ frontend/src/Api/ServerURL.tsx
@@ -1,6 +1,6 @@
 const ServerURL =
   process.env.NODE_ENV === "production"
     ? "https://container-service-1.vn5jercmc73d4.us-west-2.cs.amazonlightsail.com/api/photo_gallery"
-    : "http://localhost:8000/api/photo_gallery";
+    : "http://localhost:8000/api/";
 
 export default ServerURL;
